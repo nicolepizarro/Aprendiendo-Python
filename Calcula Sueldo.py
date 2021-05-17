3556002f08cd1b7dd085449a0d5c3067667754b0
nicolepizarro <42939297+nicolepizarro@users.noreply.github.com>
2021-05-17 16:23:57 -0300

    Delete Calcula Sueldo.py

--- Calcula Sueldo.py
+++
@@ -1,8 +0,0 @@
-## calcula el sueldo en base a un valor hora y horas trabajadas 
-
-valorHora = int(input("¿Cuánto ganas por hora de trabajo? "))
-horasTrabajadas = int(input("¿Cuántas horas trabajaste este mes? "))
-
-sueldo = valorHora * horasTrabajadas 
-
-print("Tu sueldo de este mes es " + str(sueldo))
