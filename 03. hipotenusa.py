6cc9b80eb604fd08a3b66a79a173e84486c509ae
nicolepizarro <nicolepizarrog@gmail.com>
2021-05-17 17:56:29 -0300

    ejercicio 3

---
+++ 03. hipotenusa.py
@@ -0,0 +1,8 @@
+## Hallar la longitud de la hipotenusa de un triángulo dada la medida de sus catetos
+
+a = float(input("¿Cuánto mide el cateto a del triángulo?: "))
+b = float(input("¿Cuánto mide el cateto b del triángulo?: "))  
+
+Hipotenusa = (a + b) ** 0.5 
+
+print("La hipotenusa mide " + str(Hipotenusa))
