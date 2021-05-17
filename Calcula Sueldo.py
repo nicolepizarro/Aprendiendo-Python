7f9a38b059c49ff35885a6bfa66d78281361b011
Your Name <you@example.com>
2021-05-17 12:53:09 -0300

    probando

--- Calcula Sueldo.py
+++ Calcula Sueldo.py
@@ -1,2 +1,6 @@
-saludo = "Hola, Guadalupini" 
-print(saludo)
+valorHora = int(input("¿Cuánto ganas por hora de trabajo? "))
+horasTrabajadas = int(input("¿Cuántas horas trabajaste este mes? "))
+
+sueldo = valorHora * horasTrabajadas 
+
+print("Tu sueldo de este mes es " + str(sueldo))
